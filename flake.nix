47688c0048eb6a146b311371f553c1e2ced634b6
Allen Conlon <software@conlon.dev>
2024-08-15 18:58:59 -0400

    Updated

--- flake.nix
+++ flake.nix
@@ -58,6 +58,7 @@
             };
             programs.rustfmt.enable = true;
             programs.statix.enable = true;
+            programs.yamlfmt.enable = true;
             # keep-sorted end
             settings.formatter = {
               # keep-sorted start block=yes
@@ -69,6 +70,10 @@
                 "*.json"
                 "./.github/*.json"
                 "./.vscode/*.json"
+              ];
+              yamlfmt.includes = [
+                "*.yml"
+                "*.yaml"
               ];
               # keep-sorted end
             };
--- src/main.rs
+++ src/main.rs
@@ -1,22 +1,35 @@
-use clap::Parser;
-
-#[derive(Parser, Debug)]
-#[clap(author = "Sridhar Ratnakumar", version, about)]
-/// Application configuration
-struct Args {
-    /// whether to be verbose
-    #[arg(short = 'v')]
-    verbose: bool,
-
-    /// an optional name to greet
-    #[arg()]
-    name: Option<String>,
-}
+use rand::Rng;
+use std::cmp::Ordering;
+use std::io;
 
 fn main() {
-    let args = Args::parse();
-    if args.verbose {
-        println!("DEBUG {args:?}");
+    println!("Guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+
+    loop {
+        println!("Please input your guess.");
+
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
     }
-    println!("Hello {}!", args.name.unwrap_or("world".to_string()));
 }
