35f0533ed4f41d4d7feb53069ef38ffe43649ee2
Allen Conlon <software@conlon.dev>
2024-08-26 19:37:16 -0400

    Updated

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,42 @@
-fn main() {
-    let s1: String = String::from("hello");
+use std::fmt;
+use std::clone;
 
-    let len0: usize = calculate_length(&s1);
-    let len1: usize = calculate_length(&s1);
-
-    println!("The length of '{s1}' is {len0}, {len1}.");
+struct User {
+    active: bool,
+    username: String,
+    email: String,
+    sign_in_count: u64,
 }
 
-fn calculate_length(s: &String) -> usize {
-    s.len()
+impl fmt::Display for User {
+  fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+    write!(f, "{}\t{}\t{}\t{}", self.active, self.sign_in_count, self.email, self.username)
+  }
+}
+
+impl clone::Clone for User {
+    fn clone(&self) -> Self {
+      User { active: self.active, username: self.username.clone(), email: self.email.clone(), sign_in_count: self.sign_in_count }
+    }
+
+    fn clone_from(&mut self, _source: &Self) {}
+}
+
+fn main() {
+  let user1 = User {
+    email: String::from("someone@example.com"),
+    username: String::from("someusername123"),
+    active: true,
+    sign_in_count: 1,
+  };
+
+  let user3 = user1.clone();
+
+  let user2 = User {
+    email: String::from("another@example.com"),
+    ..user1
+  };
+
+  println!("{user2}");
+  println!("{user3}");
 }
