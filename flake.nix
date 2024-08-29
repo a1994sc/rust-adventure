6ec0ea59ffa23e3629268236be3b06754974f47e
Allen Conlon <software@conlon.dev>
2024-08-29 19:39:41 -0400

    Updated flake

--- flake.nix
+++ flake.nix
@@ -7,8 +7,11 @@
       url = "github:nix-community/fenix";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+    process-compose-flake.url = "github:Platonic-Systems/process-compose-flake";
+    services-flake.url = "github:juspay/services-flake";
+    systems.url = "github:nix-systems/default";
     treefmt-nix = {
       url = "github:numtide/treefmt-nix";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,115 +20,135 @@
   };
 
   outputs =
-    {
+    inputs@{
       self,
-      fenix,
       nixpkgs,
-      flake-utils,
-      treefmt-nix,
+      ...
     }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-        f-core = fenix.packages.${system};
-        f-wasm =
-          with fenix.packages.${system};
-          combine [
-            stable.toolchain
-            targets.wasm32-unknown-unknown.stable.rust-std
-          ];
-        treefmtEval = treefmt-nix.lib.evalModule pkgs (
-          { pkgs, ... }:
-          {
-            projectRootFile = "flake.nix";
-            # keep-sorted start block=yes case=no
-            programs.dprint = {
-              enable = true;
-              settings = {
-                includes = [ "**/*.{json,md,toml}" ];
-                plugins =
-                  let
-                    dprintWasmPluginUrl = n: v: "https://plugins.dprint.dev/${n}-${v}.wasm";
-                  in
-                  [
-                    (dprintWasmPluginUrl "json" "0.19.3")
-                    (dprintWasmPluginUrl "markdown" "0.17.0")
-                    (dprintWasmPluginUrl "toml" "0.6.2")
-                  ];
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = import inputs.systems;
+      imports = [
+        inputs.process-compose-flake.flakeModule
+      ];
+      perSystem =
+        {
+          self',
+          pkgs,
+          lib,
+          system,
+          ...
+        }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            overlays = [ inputs.fenix.overlays.default ];
+          };
+          f-core = inputs.fenix.packages.${system};
+          f-wasm =
+            with inputs.fenix.packages.${system};
+            combine [
+              stable.toolchain
+              targets.wasm32-unknown-unknown.stable.rust-std
+            ];
+          treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs (
+            { pkgs, ... }:
+            {
+              projectRootFile = "flake.nix";
+              # keep-sorted start block=yes case=no
+              programs.dprint = {
+                enable = true;
+                settings = {
+                  includes = [ "**/*.{json,md,toml}" ];
+                  plugins =
+                    let
+                      dprintWasmPluginUrl = n: v: "https://plugins.dprint.dev/${n}-${v}.wasm";
+                    in
+                    [
+                      (dprintWasmPluginUrl "json" "0.19.3")
+                      (dprintWasmPluginUrl "markdown" "0.17.0")
+                      (dprintWasmPluginUrl "toml" "0.6.2")
+                    ];
+                };
               };
-            };
-            programs.jsonfmt = {
-              enable = true;
-              package = pkgs.jsonfmt;
-            };
-            programs.keep-sorted.enable = true;
-            programs.nixfmt = {
-              enable = true;
-              package = pkgs.nixfmt-rfc-style;
-            };
-            programs.rustfmt.enable = true;
-            programs.statix.enable = true;
-            # keep-sorted end
-            settings.formatter = {
-              # keep-sorted start block=yes
-              actionlint = {
-                command = pkgs.actionlint;
-                includes = [ "./.github/workflows/*.yml" ];
+              programs.jsonfmt = {
+                enable = true;
+                package = pkgs.jsonfmt;
               };
-              jsonfmt.includes = [
-                "*.json"
-                "./.github/*.json"
-                "./.vscode/*.json"
-              ];
+              programs.keep-sorted.enable = true;
+              programs.nixfmt = {
+                enable = true;
+                package = pkgs.nixfmt-rfc-style;
+              };
+              programs.rustfmt.enable = true;
+              programs.statix.enable = true;
               # keep-sorted end
-            };
-          }
-        );
-        nativeBuildInputs = with pkgs; [
-          f-core.stable.toolchain
-          pkg-config
-          # Use mold for faster linking
-          mold
-          clang
-        ];
-        buildInputs = with pkgs; [
-          openssl
-        ];
-        env = {
-          ROCKET_CLI_COLORS = "false";
-          CARGO_LINKER = "clang";
-          CARGO_RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-        };
-      in
-      {
-        packages = rec {
-          default = rust-testing;
-          rust-testing = pkgs.rustPlatform.buildRustPackage {
-            pname = "rust-testing";
-            inherit nativeBuildInputs buildInputs env;
-            inherit ((pkgs.lib.importTOML ./Cargo.toml).package) version;
-            src = ./.;
-            cargoBuildFlags = "-p rust-testing";
-
-            cargoLock = {
-              lockFile = ./Cargo.lock;
+              settings.formatter = {
+                # keep-sorted start block=yes
+                actionlint = {
+                  command = pkgs.actionlint;
+                  includes = [ "./.github/workflows/*.yml" ];
+                };
+                jsonfmt.includes = [
+                  "*.json"
+                  "./.github/*.json"
+                  "./.vscode/*.json"
+                ];
+                # keep-sorted end
+              };
+            }
+          );
+          nativeBuildInputs = with pkgs; [
+            f-core.stable.toolchain
+            pkg-config
+            # Use mold for faster linking
+            mold
+            clang
+          ];
+          buildInputs = with pkgs; [
+            openssl
+          ];
+          env = {
+            ROCKET_CLI_COLORS = "false";
+            CARGO_LINKER = "clang";
+            CARGO_RUSTFLAGS = "-Clink-arg=-fuse-ld=${pkgs.mold}/bin/mold";
+            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
+          };
+        in
+        {
+          packages = rec {
+            default = rust-testing;
+            rust-testing = pkgs.rustPlatform.buildRustPackage {
+              pname = "rust-testing";
+              inherit nativeBuildInputs buildInputs env;
+              inherit ((pkgs.lib.importTOML ./Cargo.toml).package) version;
+              src = ./.;
+              cargoBuildFlags = "-p rust-testing";
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+              };
             };
           };
+          devShells.default = pkgs.mkShell {
+            inherit nativeBuildInputs buildInputs env;
+            name = "rust";
+            # Used for development and testing
+            packages = with pkgs; [
+              process-compose
+              cargo-watch
+              nodePackages.typescript-language-server
+              vscode-langservers-extracted
+            ];
+          };
+          formatter = treefmtEval.config.build.wrapper;
+          process-compose.redis-service =
+            { config, ... }:
+            {
+              imports = [
+                inputs.services-flake.processComposeModules.default
+              ];
+              # services.redis-cluster."cluster1".enable = true;
+              services.redis."redis".enable = true;
+            };
         };
-        devShells.default = pkgs.mkShell {
-          inherit nativeBuildInputs buildInputs env;
-          name = "rust";
-          # Used for development and testing
-          packages = with pkgs; [
-            process-compose
-            cargo-watch
-            nodePackages.typescript-language-server
-            vscode-langservers-extracted
-          ];
-        };
-        formatter = treefmtEval.config.build.wrapper;
-      }
-    );
+    };
 }
