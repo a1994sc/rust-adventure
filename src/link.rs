3eb38c7fc6cd98dc432a0f24f514bdee6a312fc1
Allen Conlon <software@conlon.dev>
2024-09-02 19:32:51 -0400

    Updated to use actix

--- src/link.rs
+++ src/link.rs
@@ -1,5 +1,5 @@
 pub mod linkage {
-    use actix_web::{web, Responder, Result, HttpResponse};
+    use actix_web::{web, HttpResponse, Responder, Result};
     use serde::{Deserialize, Serialize};
 
     #[derive(Deserialize, Serialize)]
@@ -30,7 +30,7 @@ pub mod linkage {
         let pairf: f32 = encode.id as f32;
         let w: u32 = (((8.0 * pairf + 1.0).sqrt() - 1.0) / 2.0).floor() as u32;
         let t: u32 = (num::pow(w, 2) + w) / 2;
-    
+
         Ok(web::Json(Decoded {
             a: (w - (encode.id - t)),
             b: (encode.id - t),
@@ -54,26 +54,115 @@ pub mod linkage {
     pub struct Info {
         username: String,
     }
-    
+
     #[actix_web::get("/")]
     pub async fn index() -> HttpResponse {
-      HttpResponse::Ok().body("data")
+        HttpResponse::Ok().body("data")
     }
 
     #[cfg(test)]
     mod test {
-      use actix_web::{http::header::ContentType, test, App};
-      use super::*;
+        use super::*;
+        use actix_web::{
+            body::to_bytes,
+            dev::Service,
+            http::{self, header::ContentType},
+            test, App,
+        };
 
-      #[actix_web::test]
-      async fn test_index_get() {
-          let app = test::init_service(App::new().service(index)).await;
-          let req = test::TestRequest::default()
-              .insert_header(ContentType::plaintext())
-              .to_request();
-          let resp = test::call_service(&app, req).await;
-          assert!(resp.status().is_success());
-      }
+        #[actix_web::test]
+        async fn test_index_get() {
+            let app = test::init_service(App::new().service(index)).await;
+            let req = test::TestRequest::default()
+                .insert_header(ContentType::plaintext())
+                .to_request();
+            let resp = test::call_service(&app, req).await;
+            assert!(resp.status().is_success());
+        }
+
+        #[actix_web::test]
+        async fn test_linker_api_sep() {
+            let response = [
+                r#"{"a":0,"b":0}"#,
+                r#"{"a":1,"b":0}"#,
+                r#"{"a":0,"b":1}"#,
+                r#"{"a":2,"b":0}"#,
+                r#"{"a":1,"b":1}"#,
+            ];
+
+            let app =
+                test::init_service(App::new().service(web::scope("/v0").configure(scoped_config)))
+                    .await;
+
+            for (i, el) in response.iter().enumerate() {
+                let req = test::TestRequest::get()
+                    .uri(&format!("/v0/sep?id={:?}", i))
+                    .to_request();
+                let resp = app.call(req).await.unwrap();
+
+                assert_eq!(resp.status(), http::StatusCode::OK);
+                let body_bytes = to_bytes(resp.into_body()).await.unwrap();
+                assert_eq!(body_bytes, el);
+            }
+        }
+
+        #[actix_web::test]
+        async fn test_linker_api_pair() {
+            let response = [
+                [
+                    r#"{"id":0}"#,
+                    r#"{"id":2}"#,
+                    r#"{"id":5}"#,
+                    r#"{"id":9}"#,
+                    r#"{"id":14}"#,
+                ],
+                [
+                    r#"{"id":1}"#,
+                    r#"{"id":4}"#,
+                    r#"{"id":8}"#,
+                    r#"{"id":13}"#,
+                    r#"{"id":19}"#,
+                ],
+                [
+                    r#"{"id":3}"#,
+                    r#"{"id":7}"#,
+                    r#"{"id":12}"#,
+                    r#"{"id":18}"#,
+                    r#"{"id":25}"#,
+                ],
+                [
+                    r#"{"id":6}"#,
+                    r#"{"id":11}"#,
+                    r#"{"id":17}"#,
+                    r#"{"id":24}"#,
+                    r#"{"id":32}"#,
+                ],
+                [
+                    r#"{"id":10}"#,
+                    r#"{"id":16}"#,
+                    r#"{"id":23}"#,
+                    r#"{"id":31}"#,
+                    r#"{"id":40}"#,
+                ],
+            ];
+
+            let app =
+                test::init_service(App::new().service(web::scope("/v0").configure(scoped_config)))
+                    .await;
+
+            for a in 0..5 {
+                for b in 0..5 {
+                    let req = test::TestRequest::get()
+                        .uri(&format!("/v0/pair?a={:?}&b={:?}", a, b))
+                        .to_request();
+                    let resp = app.call(req).await.unwrap();
+
+                    assert_eq!(resp.status(), http::StatusCode::OK);
+                    let body_bytes = to_bytes(resp.into_body()).await.unwrap();
+                    assert_eq!(body_bytes, response[a][b]);
+                }
+            }
+        }
 
         // #[test]
         // fn pair() {
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,17 @@
-use actix_web::{web, App, HttpServer};
+use actix_web::{middleware, web, App, HttpServer};
 
 mod link;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    env_logger::init_from_env(env_logger::Env::new().default_filter_or("info"));
+
+    log::info!("starting HTTP server at http://localhost:8080");
+
     HttpServer::new(|| {
         App::new()
+            // enable logging
+            .wrap(middleware::Logger::default())
             .service(crate::link::linkage::index)
             .service(web::scope("/v0").configure(crate::link::linkage::scoped_config))
     })
