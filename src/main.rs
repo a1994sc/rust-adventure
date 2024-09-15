100fefd197f739884b2902ef1a17f4263efc78c2
Allen Conlon <software@conlon.dev>
2024-09-14 20:23:12 -0400

    Updated playground

--- src/main.rs
+++ src/main.rs
@@ -1,41 +1,36 @@
 extern crate schema_lib;
 
-// use clap::{Parser, Subcommand};
-// use schema_lib::linkage::*;
+use clap::{Parser, Subcommand};
+use schema_lib::linkage::*;
 
-// /// Simple program to greet a person
-// #[derive(Parser, Debug)]
-// #[command(author, version, about, long_about = None)]
-// #[command(propagate_version = true)]
-// struct Args {
-//     #[command(subcommand)]
-//     cmd: Commands,
-// }
+/// Simple program to greet a person
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Args {
+    #[command(subcommand)]
+    cmd: Commands,
+}
 
-// #[derive(Debug, Subcommand)]
-// enum Commands {
-//     Pair { a: u32, b: u32 },
-// }
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Function to combine two positive int, order matters
+    Pair { a: u32, b: u32 },
+    /// Function to separate any positive int into two numbers
+    Separate { id: u32 },
+}
 
 // const ZARF_SCHEMA: &str = include_str!("../schema/zarf.schema.json");
 
 fn main() {
-    // let args: Args = Args::parse();
+    let args: Args = Args::parse();
 
-    // match &args.cmd {
-    //     Commands::Pair { a, b } => {
-    //         println!("The ID is {:?}", pair(Decoded { a: *a, b: *b }).id);
-    //     }
-    // }
-
-    // for _ in 0..args.count {
-    //     println!("Hello {}!", args.name);
-    // }
-
-    // println!("{}", ZARF_SCHEMA.len());
-
-    let id: u32 = 17;
-    println!("{:?}", id as f32);
-    // println!("{:?}", Encoded { id: 368 });
-    // println!("{:?}", separate(Encoded { id: 360 }));
+    match &args.cmd {
+        Commands::Pair { a, b } => {
+            println!("{:?}", pair(Decoded { a: *a, b: *b }));
+        }
+        Commands::Separate { id } => {
+            println!("{:?}", separate(Encoded { id: *id }));
+        }
+    }
 }
