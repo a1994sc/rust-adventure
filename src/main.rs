c2f66a6b849bd4c5a891fad4c9bc0c497882b9cd
Allen Conlon <software@conlon.dev>
2024-10-25 14:49:03 -0400

    Update package

--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,44 @@
 use clap::Parser;
-use yaml_rust::{YamlEmitter, YamlLoader};
+use yaml_rust::YamlLoader;
 
 /// Search for a pattern in a file and display the lines that contain it.
 #[derive(Parser, Debug)]
 #[clap(author, version, about, long_about = None)]
 struct Flags {
-    /// The path to the file to read
+    /// The path to the package file
     #[arg(long)]
-    path: std::path::PathBuf,
+    package: std::path::PathBuf,
 }
+
+const LIST_KEY: &str = "package-image-list";
+const CHART_KEY: &str = "chart";
+const VERSION_KEY: &str = "version";
 
 fn main() {
     let flag: Flags = Flags::parse();
-    let s: String = std::fs::read_to_string(&flag.path).expect("could not read file");
+    let s: String = std::fs::read_to_string(&flag.package).expect("could not read file");
 
-    let docs: Vec<yaml_rust::Yaml> = YamlLoader::load_from_str(&s).unwrap();
+    let mut docs: Vec<yaml_rust::Yaml> = YamlLoader::load_from_str(&s).unwrap();
 
-    // Multi document support, doc is a yaml::Yaml
-    let doc: &yaml_rust::Yaml = &docs[0];
+    for doc in docs.iter_mut() {
+        let list: yaml_rust::Yaml = yaml_rust::Yaml::String(LIST_KEY.to_string());
 
-    // Debug support
-    println!("{:?}", doc);
+        if doc.as_hash().unwrap().contains_key(&list) {
+            let package: &yaml_rust::Yaml = &doc[LIST_KEY];
+            let chart: yaml_rust::Yaml = yaml_rust::Yaml::String(CHART_KEY.to_string());
+            let version: yaml_rust::Yaml = yaml_rust::Yaml::String(VERSION_KEY.to_string());
 
-    // Dump the YAML object
-    let mut out_str: String = String::new();
-    {
-        let mut emitter: YamlEmitter<'_> = YamlEmitter::new(&mut out_str);
-        emitter.dump(doc).unwrap(); // dump the YAML object to a String
+            for key in package.as_hash().unwrap().keys() {
+                let pkg = package.as_hash().unwrap().get(key).unwrap();
+
+                if pkg.as_hash().unwrap().contains_key(&version) {
+                    println!("{:?}", pkg[VERSION_KEY].as_str().unwrap());
+                }
+
+                if pkg.as_hash().unwrap().contains_key(&chart) {
+                    println!("{:?}", pkg[CHART_KEY].as_str().unwrap());
+                }
+            }
+        }
     }
-    println!("{}", out_str);
 }
